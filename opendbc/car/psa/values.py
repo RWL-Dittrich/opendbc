d27f5ea5322fd089c385cd871569444415fa7c49
Robin Dittrich <r.dittrich@niverplast.com>
2026-05-07 14:09:59 +0200

    tune steerratio

--- opendbc/car/psa/values.py
+++ opendbc/car/psa/values.py
@@ -36,7 +36,7 @@ class PSAPlatformConfig(PlatformConfig):
 class CAR(Platforms):
   PSA_PEUGEOT_208 = PSAPlatformConfig(
     [PSACarDocs("Peugeot 208 2019-25")],
-    CarSpecs(mass=1530, wheelbase=2.54, steerRatio=17.9),
+    CarSpecs(mass=1530, wheelbase=2.54, steerRatio=18.3),
   )
   PSA_PEUGEOT_508 = PSAPlatformConfig(
     [PSACarDocs("Peugeot 508 2019-23")],
